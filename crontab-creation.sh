7fd0b0e3a214defc5ac983f8dc54ad13503aacf7
Nik <nicolas.bellotti@edu-its.it>
2025-05-19 22:31:05 +0200

    Update crontab-creation.sh
    Fixed an error and added comments

--- crontab-creation.sh
+++ crontab-creation.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#create the directory to store backups
 sudo mkdir /opt/backup-OtW;
 
-{ crontab -l -u $(whoami); echo '59 23 * * * /home/niktus/backup-week.sh '; } | crontab -u user -;
+USER=$(whoami)
+
+# scheduling the backup every a day at 23.59 
+{ crontab -l -u $(USER); echo '59 23 * * * /home/niktus/backup-week.sh '; } | crontab -u $(USER) -;
